d72178e742ca570c476dfdf071f002074c4abaf1
Sebastian Richter <sebastian.max.richter@gmail.com>
2021-10-18 13:54:31 +0200

    Remove build folder

--- build/controllers/TransportActivityController.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TransportActivityController_1 = require("./TransportActivityController");
-describe(TransportActivityController_1.TransportActivityController.name, () => {
-    it.todo("should work");
-});
